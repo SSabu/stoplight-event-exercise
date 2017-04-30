f2ba0f1cb73707fb1f5c6389af27155a441926b3
SSabu <sandeepsabu09@gmail.com>
2017-04-29 20:11:06 -0700

    adding stoplights exercise

--- index.js
+++ index.js
@@ -1,5 +1,72 @@
-(function() {
+(function(window, document) {
   'use strict';
 
   // YOUR CODE HERE
-})();
+
+  /*
+     Selectors
+  */
+
+  let stopControl = document.querySelector('#stopButton');
+  let stopLight = document.querySelector('#stopLight');
+  let slowControl = document.querySelector('#slowButton');
+  let slowLight = document.querySelector('#slowLight');
+  let goControl = document.querySelector('#goButton');
+  let goLight = document.querySelector('#goLight');
+
+  let buttons = document.querySelectorAll('.button');
+  let lights = document.querySelectorAll('.bulb');
+
+
+  /*
+     Methods
+  */
+
+
+  function changeToRed(event){
+    if(stopLight.className.includes('stop')){
+      stopLight.className = "bulb";
+      stopControl.className = "button";
+    } else {
+      stopLight.className += " stop";
+      stopControl.className +=" on";
+    }
+  }
+
+  function changeToGreen(event){
+    if(goLight.className.includes('go')){
+      goLight.className = "bulb";
+    } else {
+      goLight.className += " go";
+    }
+  }
+
+  function changeToYellow(event){
+    if(slowLight.className.includes('slow')){
+      slowLight.className = "bulb";
+    } else {
+      slowLight.className +=" slow";
+    }
+  }
+
+  function buttonEnteredAlert(event){
+    console.log("Entered "+event.target.innerHTML+" Button");
+  };
+
+  function buttonLeftAlert(event){
+    console.log("Left "+event.target.innerHTML+" Button");
+  };
+
+  /*
+     Events
+  */
+
+  stopControl.addEventListener('click', changeToRed);
+  slowControl.addEventListener('click', changeToYellow);
+  goControl.addEventListener('click', changeToGreen);
+
+  buttons.forEach((button) => button.addEventListener('mouseenter', buttonEnteredAlert));
+
+  buttons.forEach((button) => button.addEventListener('mouseleave', buttonLeftAlert));
+
+})(window, document);
